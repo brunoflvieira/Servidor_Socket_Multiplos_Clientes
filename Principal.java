878351d92609d7ba2da694a04a6591938bec1b18
Bruno Flores <117491562+brunoflvieira@users.noreply.github.com>
2023-04-18 20:16:17 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,24 @@
+package Principal;
+
+
+import java.net.*;
+import java.io.*;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket servidor = new ServerSocket(4444);
+            System.out.println("Aguardando cliente!");
+            while (true) {
+                Socket socket = servidor.accept(); // espera
+                Servidor cliente;
+                cliente = new Servidor(socket);
+                System.out.println("Chegou um cliente!");
+                cliente.start();
+            }
+        } catch (IOException e) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
